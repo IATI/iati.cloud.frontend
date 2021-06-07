8e44a44506bcd256fffbd50fed03c43fd170d612
akmiller01 <akmiller01@users.noreply.github.com>
2021-06-07 20:09:35 +0000

    deploy: b4eef99c6d0d55eef3eaca4aaf543a78bcd8d437

---
+++ precache-manifest.56c17614f5153a964657980970ba9988.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "028489e89a90aed50e5a3492a9f4ad19",
+    "url": "/iati.cloud.frontend/index.html"
+  },
+  {
+    "revision": "29f56b9c4384df14c2ec",
+    "url": "/iati.cloud.frontend/static/css/main.b9c24122.chunk.css"
+  },
+  {
+    "revision": "65a97f918a4ebb432681",
+    "url": "/iati.cloud.frontend/static/js/2.857b0007.chunk.js"
+  },
+  {
+    "revision": "6e26179e70fc7f4e92766299aebc5bbb",
+    "url": "/iati.cloud.frontend/static/js/2.857b0007.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a49bba9e691871e524ec",
+    "url": "/iati.cloud.frontend/static/js/3.21eaaf5a.chunk.js"
+  },
+  {
+    "revision": "d9593fda48a17fe676aeeb5e47433ada",
+    "url": "/iati.cloud.frontend/static/js/3.21eaaf5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2546f721e68770c2a1a8",
+    "url": "/iati.cloud.frontend/static/js/4.6df20b49.chunk.js"
+  },
+  {
+    "revision": "29f56b9c4384df14c2ec",
+    "url": "/iati.cloud.frontend/static/js/main.536b9a52.chunk.js"
+  },
+  {
+    "revision": "4e0827493027b8dc6519",
+    "url": "/iati.cloud.frontend/static/js/runtime-main.5955b42e.js"
+  },
+  {
+    "revision": "cf294318467a6dfaa54091b1e154dec1",
+    "url": "/iati.cloud.frontend/static/media/404_image.cf294318.png"
+  },
+  {
+    "revision": "210fb4febe9d77130340b6ff755fcffd",
+    "url": "/iati.cloud.frontend/static/media/DottedMap.210fb4fe.svg"
+  },
+  {
+    "revision": "a0ecf0ea637728c9a86e4cb67a6e4930",
+    "url": "/iati.cloud.frontend/static/media/logo_zz.a0ecf0ea.png"
+  },
+  {
+    "revision": "55cc2d2f67d3a5f75c9a1ee126010c82",
+    "url": "/iati.cloud.frontend/static/media/subset-Inter-LightBETA.55cc2d2f.eot"
+  },
+  {
+    "revision": "5cd69af32b078f08c70bbd9c2156e8b0",
+    "url": "/iati.cloud.frontend/static/media/subset-Inter-LightBETA.5cd69af3.svg"
+  },
+  {
+    "revision": "9e7771db827f06ed1fb7b28651fa6a95",
+    "url": "/iati.cloud.frontend/static/media/subset-Inter-LightBETA.9e7771db.ttf"
+  },
+  {
+    "revision": "a8cfa1471d25a3393b40e740e2d8a2ea",
+    "url": "/iati.cloud.frontend/static/media/subset-Inter-LightBETA.a8cfa147.woff"
+  },
+  {
+    "revision": "f111546356c86b00fa2c311722c396cd",
+    "url": "/iati.cloud.frontend/static/media/subset-Inter-LightBETA.f1115463.woff2"
+  },
+  {
+    "revision": "4b8134296b60828af2c1ed52a060f4a3",
+    "url": "/iati.cloud.frontend/static/media/subset-Inter-Medium.4b813429.svg"
+  },
+  {
+    "revision": "546ccfea9acc3071eb6e1b151fcbf904",
+    "url": "/iati.cloud.frontend/static/media/subset-Inter-Medium.546ccfea.eot"
+  },
+  {
+    "revision": "68fadc1888a88af75d70f2528ea6d5b8",
+    "url": "/iati.cloud.frontend/static/media/subset-Inter-Medium.68fadc18.ttf"
+  },
+  {
+    "revision": "8ad9623ccc8fefd5d1f30ee22f47a933",
+    "url": "/iati.cloud.frontend/static/media/subset-Inter-Medium.8ad9623c.woff"
+  },
+  {
+    "revision": "bacf796287d89d5468d1c0c9d844cb28",
+    "url": "/iati.cloud.frontend/static/media/subset-Inter-Medium.bacf7962.woff2"
+  },
+  {
+    "revision": "0de8aa6d544f1b57caacbf9e0983a0f0",
+    "url": "/iati.cloud.frontend/static/media/subset-Inter-Regular.0de8aa6d.eot"
+  },
+  {
+    "revision": "272c86b443c34d5be1c2f6a30c8e72b3",
+    "url": "/iati.cloud.frontend/static/media/subset-Inter-Regular.272c86b4.woff2"
+  },
+  {
+    "revision": "6ef2be6c770ebafff55afcd9046f1b33",
+    "url": "/iati.cloud.frontend/static/media/subset-Inter-Regular.6ef2be6c.ttf"
+  },
+  {
+    "revision": "72c13fe5733c5e7663c901effa990244",
+    "url": "/iati.cloud.frontend/static/media/subset-Inter-Regular.72c13fe5.woff"
+  },
+  {
+    "revision": "b282b93b9437be80872799891169ae2c",
+    "url": "/iati.cloud.frontend/static/media/subset-Inter-Regular.b282b93b.svg"
+  },
+  {
+    "revision": "1ce970a4a42c1ce9ff2e543f3be0fda9",
+    "url": "/iati.cloud.frontend/static/media/subset-Inter-SemiBold.1ce970a4.eot"
+  },
+  {
+    "revision": "3b9409a84aa09ac13ca069a91dabe3a0",
+    "url": "/iati.cloud.frontend/static/media/subset-Inter-SemiBold.3b9409a8.woff"
+  },
+  {
+    "revision": "836006a8a31a532414d7813888567833",
+    "url": "/iati.cloud.frontend/static/media/subset-Inter-SemiBold.836006a8.ttf"
+  },
+  {
+    "revision": "8d053c73cfb0d06c836908b19eb4a631",
+    "url": "/iati.cloud.frontend/static/media/subset-Inter-SemiBold.8d053c73.svg"
+  },
+  {
+    "revision": "9bfca269177494ee0b0b39945a6af472",
+    "url": "/iati.cloud.frontend/static/media/subset-Inter-SemiBold.9bfca269.woff2"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2e81cb539e912165f73fc06758c69997.js"
+  "/iati.cloud.frontend/precache-manifest.56c17614f5153a964657980970ba9988.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/iati.cloud.frontend/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
